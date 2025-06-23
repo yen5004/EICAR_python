97537f05ddd7352b63c2f8162806f521f7e3d713
yen5004 <119946331+yen5004@users.noreply.github.com>
2025-06-23 09:00:34 -0400

    Create 3A_In-Memory-Only_EICAR.py

---
+++ 3A_In-Memory-Only_EICAR.py
@@ -0,0 +1,10 @@
+# In-Memory-Only EICAR Execution
+# This simulates malware that operates only in memory — no file drop — by writing the EICAR string into a BytesIO or subprocess without saving it.
+
+import subprocess
+
+eicar = "X5O!P%@AP[4\\PZX54(P^)7CC)7}$EICAR-STANDARD-ANTIVIRUS-TEST-FILE!$H+H*"
+cmd = f'echo {eicar}'
+
+# AVs may catch this command activity
+subprocess.run(cmd, shell=True)
