946da914113780bc5d540c6c51701e06e00f6545
yen5004 <119946331+yen5004@users.noreply.github.com>
2025-07-09 17:09:47 -0400

    Update and rename 3B_In-Memory-Only_EICAR.py to 5_Tempfile_EICAR.py

--- 5_Tempfile_EICAR.py
+++ 5_Tempfile_EICAR.py
@@ -1,15 +1,19 @@
+#!/usr/bin/env python3
+
 # Use tempfile, auto-delete file after use. This mimics a dropper that writes to disk briefly.
 # Depending on AV configuration, detection might occur on write or not at all (especially if detection is on execution or after a scheduled scan).
+# Can add autodelete if required
 
 import tempfile
 import os
 
 eicar = "X5O!P%@AP[4\\PZX54(P^)7CC)7}$EICAR-STANDARD-ANTIVIRUS-TEST-FILE!$H+H*"
 
-with tempfile.NamedTemporaryFile(delete=False, suffix=".com") as tmp:
+with tempfile.NamedTemporaryFile(delete=False, suffix=".txt") as tmp:
     tmp.write(eicar.encode())
     tmp.flush()
     print(f"Wrote to {tmp.name}")
 
 # Manually delete, or rely on system cleanup
-os.remove(tmp.name)
+#os.remove(tmp.name)
+print("5_Tempfile_EICAR test complete")
