0743bf5685a85b4c70e152255e6d45d45accadcc
yen5004 <119946331+yen5004@users.noreply.github.com>
2025-07-09 13:26:10 -0400

    Update 1_Basic_EICAR_Dropper.py

--- 1_Basic_EICAR_Dropper.py
+++ 1_Basic_EICAR_Dropper.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 
 # 1. Basic Python Dropper
-# This script simply writes the EICAR string to a file "eicars.com"
+# This script simply writes the EICAR string to a file "eicar_basic_dropper.txt"
 EICAR_STRING = "X5O!P%@AP[4\\PZX54(P^)7CC)7}$EICAR-STANDARD-ANTIVIRUS-TEST-FILE!$H+H*"
 
 with open("eicar_basic_dropper.txt", "w") as f:
     f.write(EICAR_STRING)
 
 #print({EICAR_STRING})
-print("Basic_EICAR_Dropper testing complete. Look for eicars.com file in directory.")
+print("Basic_EICAR_Dropper testing complete. Look for eicar_basic_dropper.txt file in directory.")
