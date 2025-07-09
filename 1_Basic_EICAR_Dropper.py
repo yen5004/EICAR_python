bc8767c51d05ffb8f0dbe9e28ed0758509cc7399
yen5004 <119946331+yen5004@users.noreply.github.com>
2025-07-09 13:24:02 -0400

    Update 1_Basic_EICAR_Dropper.py

--- 1_Basic_EICAR_Dropper.py
+++ 1_Basic_EICAR_Dropper.py
@@ -1,15 +1,11 @@
 #!/usr/bin/env python3
 
 # 1. Basic Python Dropper
-# This script simply writes the EICAR string to a file \u2014 the AV should catch this instantly. Run this \u2014 expect immediate AV detection.
-
-
+# This script simply writes the EICAR string to a file "eicars.com"
 EICAR_STRING = "X5O!P%@AP[4\\PZX54(P^)7CC)7}$EICAR-STANDARD-ANTIVIRUS-TEST-FILE!$H+H*"
 
 with open("eicar.com", "w") as f:
     f.write(EICAR_STRING)
 
-print("writing  complete")
-print({EICAR_STRING})
-print("all test complete")
-
+#print({EICAR_STRING})
+print("Basic_EICAR_Dropper testing complete. Look for eicars.com file in directory.")
