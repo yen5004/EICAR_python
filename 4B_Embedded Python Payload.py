bfee12bd87f09a88f83173fba00bcf24e691e4aa
yen5004 <119946331+yen5004@users.noreply.github.com>
2025-07-09 15:22:31 -0400

    Update 4B_Embedded Python Payload.py

--- 4B_Embedded Python Payload.py
+++ 4B_Embedded Python Payload.py
@@ -1,11 +1,13 @@
+#!/usr/bin/env python3
+
 # Here’s what the payload might do before it’s base64-encoded:
 import base64
 
-eicar = base64.b64decode("WDVPKVB...").decode()
-with open("eicar.com", "w") as f:
+eicar = base64.b64decode("WDVPIVAlQEFQWzRcUFpYNTRQXl4pN0NDKTd9JEVJQ0FSLVNUQU5EQVItQU5USVZJUlVTLVRFU1QtRklMRSFIK0gq").decode()
+with open("4B_eicar_test_file.txt", "w") as f:
     f.write(eicar)
 
 # Then you Base64-encode this whole Python snippet and embed it into the VBA macro.
 # You can encode it like this from your terminal:
-# echo "import base64; eicar=base64.b64decode('...'); open('eicar.com','w').write(eicar)" | base64
+# echo "import base64; eicar=base64.b64decode('WDVPIVAlQEFQWzRcUFpYNTRQXl4pN0NDKTd9JEVJQ0FSLVNUQU5EQVItQU5USVZJUlVTLVRFU1QtRklMRSFIK0gq'); open('4B_eicar_test_file.txt','w').write(eicar)" | base64
 # Use the output in the VBA code.
