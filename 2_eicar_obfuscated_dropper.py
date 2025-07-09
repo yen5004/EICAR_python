084f04cad3876019cb597cc4d2727d6acbeb5f18
yen5004 <119946331+yen5004@users.noreply.github.com>
2025-07-09 14:00:39 -0400

    Update 2_eicar_obfuscated_dropper.py

--- 2_eicar_obfuscated_dropper.py
+++ 2_eicar_obfuscated_dropper.py
@@ -1,13 +1,16 @@
+#!/usr/bin/env python3
+
 # This simulates how real malware hides payloads — encoded, split, or disguised — and dynamically decodes and drops them.
 # You can experiment by splitting the string, compressing it, etc. Detection may or may not trigger — a good way to test behavioral AV.
+# File will be created named "eicar_obf.txt"
 
 import base64
 
 # Base64-encoded EICAR string
-encoded = "WDVPKVB%QVBbNFxQWlg1NChQXildN0NDKTd9JEVJQ0FSLVNUQU5EQVItQU5USVZJUlVTLVRFU1QtRklMRSFKSEor"
+encoded = "WDVPIVAlQEFQWzRcUFpYNTRQXl4pN0NDKTd9JEVJQ0FSLVNUQU5EQVItQU5USVZJUlVTLVRFU1QtRklMRSFIK0gq"
 
 # Decode and write to file
 decoded = base64.b64decode(encoded).decode('ascii')
 
-with open("eicar_obf.com", "w") as f:
+with open("eicar_obf.txt", "w") as f:
     f.write(decoded)
