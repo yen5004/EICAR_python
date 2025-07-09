7137ea5d64a577af930d2c0276ea9122a6d0c1a7
yen5004 <119946331+yen5004@users.noreply.github.com>
2025-07-09 15:39:35 -0400

    Update and rename 1_Basic_EICAR_Dropper.py to 1_Basic_Python_Dropper.py

--- 1_Basic_Python_Dropper.py
+++ 1_Basic_Python_Dropper.py
@@ -2,6 +2,9 @@
 
 # 1. Basic Python Dropper
 # This script simply writes the EICAR string to a file "eicar_basic_dropper.txt"
+# Purpose: Statick AV signature test
+# Detection Chance: 100% if AV active
+
 EICAR_STRING = "X5O!P%@AP[4\\PZX54(P^)7CC)7}$EICAR-STANDARD-ANTIVIRUS-TEST-FILE!$H+H*"
 
 with open("eicar_basic_dropper.txt", "w") as f:
