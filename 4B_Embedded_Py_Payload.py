2e441f396075b403a6f2626ecfaf7c23859ad2dd
yen5004 <119946331+yen5004@users.noreply.github.com>
2025-07-09 15:26:15 -0400

    Update and rename 4B_Embedded Python Payload.py to 4B_Embedded_Py_Payload.py

--- 4B_Embedded_Py_Payload.py
+++ 4B_Embedded_Py_Payload.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
 
-# Here’s what the payload might do before it’s base64-encoded:
+# Heres what the payload might do before its base64-encoded:
+
 import base64
 
 eicar = base64.b64decode("WDVPIVAlQEFQWzRcUFpYNTRQXl4pN0NDKTd9JEVJQ0FSLVNUQU5EQVItQU5USVZJUlVTLVRFU1QtRklMRSFIK0gq").decode()
+
 with open("4B_eicar_test_file.txt", "w") as f:
     f.write(eicar)
 
