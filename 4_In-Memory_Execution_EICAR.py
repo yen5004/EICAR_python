ff23ff66fd0382086d1a6631bafa1ea2118d4067
yen5004 <119946331+yen5004@users.noreply.github.com>
2025-07-09 16:48:16 -0400

    Update and rename 4_In-Memory-Only_EICAR.py to 4_In-Memory_Execution_EICAR.py

--- 4_In-Memory_Execution_EICAR.py
+++ 4_In-Memory_Execution_EICAR.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
-# In-Memory-Only EICAR Execution
-# This simulates malware that operates only in memory \u2014 no file drop \u2014 by writing the EICAR string into a BytesIO or subprocess without saving it.
+# In-Memory-Execution EICAR
+# This simulates malware that operates only in memory, no file drop by writing the EICAR string into a BytesIO or subprocess without saving it.
 # Upon completion of running this script the eicars text should be displayed on screen.
 
 import subprocess
